cee28aa56599ae554128eadc920b76ca503fd648
halilakcil <halilakcil@gmail.com>
2022-03-02 12:47:18 +0300

    refactor

--- HotelHubAPI/Program.cs
+++ HotelHubAPI/Program.cs
@@ -9,12 +9,23 @@ using Hotel.Core.Utilities.Security.JWT;
 using Hotel.Entites.Maps;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks().AddSqlServer(
+    @"Server=(localdb)\MSSQLLocalDB;Database=HotelHubDb;Trusted_Connection=true",
+    "SELECT 1;",
+    $"Sql => {@"Server=(localdb)\MSSQLLocalDB;Database=HotelHubDb;Trusted_Connection=true".Split(";").First()}",
+    HealthStatus.Degraded,
+    timeout: TimeSpan.FromSeconds(30),
+    tags: new[] { "db", "sql", "sqlServer", }
+    );
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -49,9 +60,26 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.ConfigureCustomExceptionMiddleware();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseHealthChecks("/hc", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+{
+    ResponseWriter = async (c, r) =>
+    {
+        c.Response.ContentType = "application/json";
+
+        var result = JsonConvert.SerializeObject(new
+        {
+            status = r.Status.ToString(),
+            components = r.Entries.Select(e => new { key = e.Key, value = e.Value })
+        });
+
+        await c.Response.WriteAsync(result);
+    }
+});
 
 app.MapControllers();
 
